3b8e9dc41ef96c07c9825e4ef13002156c5211d0
Hyun Min Choi <hmchoi@berkeley.edu>
2021-02-28 00:53:20 -0800

    stuck on addTask function

---
+++ src/ToDoForm.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import {addtask} from "./App";
+const ToDoForm = ({addTask}) => {
+    const [userInput, setUserInput] = useState('');
+   
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value)
+}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTask(userInput);
+        setUserInput("");
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input value={userInput} type="text" onChange={handleChange} placeholder=""/>
+            <button>Submit</button>
+        </form>
+    );
+};
+
+export default ToDoForm;
+
