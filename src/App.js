d54b67a888b91c29b03b4e7526de8d5d9b489182
Hyun Min Choi <hmchoi@berkeley.edu>
2021-02-27 22:19:45 -0800

    made first version

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import Header from "./Header";
+import data from "./data.json";
+import ToDoList from "./ToDoList";
+
+import './App.css';
+
+function App() {
+  const [ toDoList, setToDoList ] = useState(data);
+  return (
+    <div className="App">
+      <Header />
+      <ToDoList toDoList={toDoList}/>
+    </div>
+  );
+}
+
+export default App;
